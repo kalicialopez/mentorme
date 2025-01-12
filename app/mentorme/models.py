430911ffe4c2dd747705e00e2f1e017a427b140b
Kai Striega <me@kaistriega.com>
2025-01-12 13:50:16 +1100

    Refactor choices to be defined inside Models

--- app/mentorme/models.py
+++ app/mentorme/models.py
@@ -3,27 +3,21 @@ from django.contrib.gis.db import models as gis_models
 from django.db import models
 
 
-class SkillProficiencyLevel(models.IntegerChoices):
-    FUNDAMENTAL = 1, "Fundamental"
-    NOVICE = 2, "Novice"
-    INTERMEDIATE = 3, "Intermediate"
-    ADVANCED = 4, "Advanced"
-    EXPERT = 5, "Expert"
-
-
-class SessionMode(models.IntegerChoices):
-    IN_PERSON = 1, "In Person"
-    VIRTUAL = 2, "Virtual"
-
-
 class Skill(models.Model):
     name = models.CharField(max_length=100)
 
 
 class UserSkill(models.Model):
+    class Proficiency(models.IntegerChoices):
+        FUNDAMENTAL = 1, "Fundamental"
+        NOVICE = 2, "Novice"
+        INTERMEDIATE = 3, "Intermediate"
+        ADVANCED = 4, "Advanced"
+        EXPERT = 5, "Expert"
+
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     skill = models.ForeignKey(Skill, on_delete=models.CASCADE)
-    proficiency = models.IntegerField(choices=SkillProficiencyLevel.choices)
+    proficiency = models.IntegerField(choices=Proficiency.choices)
 
 
 class UserProfile(models.Model):
@@ -35,12 +29,16 @@ class UserProfile(models.Model):
 
 
 class MentorshipSession(models.Model):
+    class Mode(models.IntegerChoices):
+        IN_PERSON = 1, "In Person"
+        VIRTUAL = 2, "Virtual"
+
     mentee = models.ForeignKey(User, on_delete=models.CASCADE)
     mentor = models.ForeignKey(User, on_delete=models.CASCADE)
 
     scheduled_time = models.DateTimeField()
     duration = models.DurationField()
-    mode = models.SmallIntegerField(choices=SessionMode.choices)
+    mode = models.SmallIntegerField(choices=Mode.choices)
 
 
 class MentorshipSessionReview(models.Model):
@@ -48,8 +46,15 @@ class MentorshipSessionReview(models.Model):
         MENTEE = "Mentee"
         MENTOR = "Mentor"
 
+    class Rating(models.IntegerChoices):
+        POOR = 1, "Poor"
+        FAIR = 2, "Fair"
+        GOOD = 3, "Good"
+        VERY_GOOD = 4, "Very Good"
+        EXCELLENT = 5, "Excellent"
+
     session = models.ForeignKey(MentorshipSession, on_delete=models.CASCADE)
     reviewer = models.ForeignKey(User, on_delete=models.CASCADE)
     role = models.TextField(choices=Role.choices)
-    rating = models.IntegerField()
+    rating = models.IntegerField(choices=Rating.choices)
     comment = models.TextField()
